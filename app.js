946c547fa8594e24569290050a9612f5e77be663
Arnav-Naik <73340433+Arnav-Naik@users.noreply.github.com>
2020-12-19 16:31:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const play = document.getElementById("play-button");
+
+play.onclick=()=>{
+location.href="./game.html";
+console.log("test passed");
+};  
+var number1=math.round(Math.random()*100);
